8df8de1e950bb5bdb3ccf9f522eabd71128ac765
jayalaxmipatil0901-cell <jayalaxmipatil0901@gmail.com>
2026-03-20 12:20:48 +0530

    Add files via upload

---
+++ .2.py
@@ -0,0 +1,9 @@
+t1=(0,)#A one -item tuple(not an expression)
+t2=(0, 1,2,3)#A four item tuple
+t3=0,1,2,3#some as above withot parenthesis
+t3=("abc",("def","ghi"))#nested tuple
+print(t1[0])
+print(t3[1:3])#slice
+print(len(t3))#length
+tup=(1,"a","string",1+2)
+print(tup)
